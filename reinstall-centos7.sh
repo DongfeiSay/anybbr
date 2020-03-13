3f39c1ee1769d545517cd67356d3834ec4b70619
wuntel <61235395+wuntel@users.noreply.github.com>
2020-03-13 21:58:43 +0800

    Update reinstall-centos7.sh

--- reinstall-centos7.sh
+++ reinstall-centos7.sh
@@ -1,56 +1,74 @@
 #!/bin/bash
-image='https://github.com/wuntel/anybbr/raw/master/centos-7.7-x86_64-docker.tar.xz'
-bbx='https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64'
-nc_root='/centos7'
-download(){
-if command -v wget >/dev/null 2>&1 ;then
-	mkdir $nc_root
-	wget -O "$nc_root/centos7.tar.xz" $image
-	wget -O "$nc_root/busybox" $bbx
-	chmod 777 "$nc_root/busybox"
-	else echo "ERROR:请安装wget";exit
+if command -v wget >/dev/null 2>&1;then
+	else echo "ERROR:  please install wget"
 fi
+############################################
+dl(){
+	local image='https://github.com/wuntel/anybbr/raw/master/centos-7.7-x86_64-docker.tar.xz'
+		local busybox='https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64'
+			[ -d /tmpos ] || mkdir /tmpos
+				wget -qO /tmpos/centos7.tar.xz ${image} && wget -qO /tmpos/busybox ${busybox} && chmod 777 /tmpos/busybox
 }
-del_all(){
-	cp /etc/fstab $nc_root
+############################################
+backup-data(){
+	cp /etc/resolv.conf /etc/fstab /tmpos
+}
+############################################
+del-root(){
 	if command -v chattr >/dev/null 2>&1; then
-		find / -type f \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path "$nc_root/*" \) \
+		find / -type f \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path '/selinux/*' -and ! -path "/tmpos/*" \) \
 			-exec chattr -i {} + 2>/dev/null || true
 	fi
-	find / \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path "$nc_root/*" \) -delete 2>/dev/null || true
+		find / \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path '/selinux/*' -and ! -path "/tmpos/*" \) -delete 2>/dev/null || true
 }
-extract_image(){
-	xzcat="$nc_root/busybox xzcat"
-	tar="$nc_root/busybox tar"
-	$xzcat "$nc_root/centos7.tar.xz" | $tar -x -C /
-	mv $nc_root/fstab /etc 
+############################################
+un-xz(){
+	local tar='/tmpos/busybox tar'
+	local xzcat='/tmpos/busybox xzcat'
+		${xzcat} /tmpos/centos7.tar.xz | ${tar} -x -C /
 }
-install_package(){
-	echo "nameserver 8.8.8.8" > /etc/resolv.conf
-	yum install grub2 dhclient openssh-server passwd kernel dracut-network -y || true
-	sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config
-	systemctl enable sshd
-	read -p "请修改root密码:" password
-	echo $password | passwd --stdin root
+############################################
+restore-data(){
+	mv /tmpos/resolv.conf /tmpos/fstab /etc
+	##chpasswd
+		echo "root:cnddy10" | chpasswd
 }
-install_grub_netcfg(){
-	device=$(lsblk -npsro TYPE,NAME  | awk '($1 == "disk") { print $2}' | head -n1)
-	grub2-install $device
-	echo -e "GRUB_TIMEOUT=5\nGRUB_CMDLINE_LINUX=\"net.ifnames=0 console=ttyS0\"" > /etc/default/grub
-	grub2-mkconfig -o /boot/grub2/grub.cfg 2>/dev/null
+############################################
+installer-pak(){
+	yum -y install grub2 dhclient openssh-server kernel dracut-network || true
+		##enable sshd
+			sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config && systemctl enable sshd
+}
+############################################
+create-kernel-image(){
+	dracut -fNm "bash base kernel-modules rootfs-block network shutdown" --kver "3.10.0-1062.12.1.el7.x86_64" 2>/dev/null
+}
+############################################
+config-network(){
 	touch /etc/sysconfig/network
-	cat<<eof>/etc/sysconfig/network-scripts/ifcfg-eth0
+		route=$(ip route get 1.1.1.1 | awk '{print $3}')
+		ip=$(ip -o -4 a|grep -Ev  '\s(docker|lo)'|awk '{print $4}'|cut -d/ -f1)
+			cat >/etc/sysconfig/network-scripts/ifcfg-eth0 <<EOF
 DEVICE=eth0
-BOOTPROTO=dhcp
+BOOTPROTO=static
 ONBOOT=yes
-eof
+IPADDR=$ip
+PREFIX=8
+GATEWAY=$route
+DNS1=8.8.8.8
+DNS2=1.1.1.1
+EOF
 }
-download
-del_all
-extract_image
-install_package
-install_grub_netcfg
-rm -rf $nc_root
-yum clean all
-dracut -fNm "bash base kernel-modules rootfs-block network shutdown" --kver "3.10.0-1062.12.1.el7.x86_64" 2>/dev/null
-echo -e "\n please run \"sync;reboot -f\" \n"
+###########################################
+config-grub(){
+	local dev=$(ls /sys/block)
+		grub2-install ${dev}
+			grub2-mkconfig -o /boot/grub2/grub.cfg
+}
+###########################################
+echo "
+
+Install centos7 successfully  ;     default root password:  cnddy10
+
+please run sync;reboot -f
+"
